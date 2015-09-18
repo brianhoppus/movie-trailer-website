fcd5286648647df26c708b1e0a84d22483a42955
Brian Hoppus <brian@hoppus.io>
2015-09-18 15:59:30 -0700

    add more Movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,22 +1,40 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "http://upload.wikmedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "http://cdn.hellogiggles.com/wp-content/uploads/2013/07/05/toystory.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-print(toy_story.storyline)
-
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "http://upload.wikmedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                     "https://www.youtube.com/watch?v=-9ceBgWV8io")
-
-print(avatar.storyline)
+avatar = media.Movie("Jurassic Park",
+                     "Man creates dinosaur, dinosaur eats man",
+                     "http://www.movieartarena.com/imgs/jurassicparkay.jpg",
+                     "https://www.youtube.com/watch?v=sgjDSfbB2ok")
 
 the_empire_strikes_back = media.Movie("The Empire Strikes Back",
                                       "Luke Skywalker battles Darth Vader",
                                       "https://mattadavid.files.wordpress.com/2015/09/url-6.jpg",
                                       "https://www.youtube.com/watch?v=mz_YWNhKOkM")
 
-the_empire_strikes_back.show_trailer()
+ghostbusters = media.Movie("Ghostbusters",
+                           "A team of scientists battle the supernatural for money",
+                           "http://www.moviesontheroof.com/2010/images/ghostbusters.jpg",
+                           "https://www.youtube.com/watch?v=vntAEVjPBzQ")
+
+beetlejuice = media.Movie("Beetlejuice",
+                          "A couple find themselves stuck haunting their country residence",
+                          "http://www.joblo.com/posters/images/full/1988-beetlejuice-poster1.jpg",
+                          "https://www.youtube.com/watch?v=2hovKm9oFiM")
+
+city_slickers = media.Movie("City Slickers",
+                            "Three friends find themselves as they drive cattle",
+                            "http://www.impawards.com/1991/posters/city_slickers_xlg.jpg",
+                            "https://www.youtube.com/watch?v=rpxVp1g8xMQ")
+
+movies = [toy_story,
+          avatar,
+          the_empire_strikes_back,
+          ghostbusters,
+          beetlejuice,
+          city_slickers]
+fresh_tomatoes.open_movies_page(movies)
