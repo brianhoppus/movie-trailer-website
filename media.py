10923efa5bf88a4fbd6dc4563046a7db691a3dfe
Brian Hoppus <brian@hoppus.io>
2015-09-19 08:58:45 -0700

    Fix oject class typo

--- media.py
+++ media.py
@@ -1,6 +1,6 @@
 import webbrowser
 
-class Movie(Object):
+class Movie(object):
     """ This class provides a way to store movie related information"""
 
     def __init__(self, movie_title, movie_storyline, poster_image,
