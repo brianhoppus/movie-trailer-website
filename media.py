cc78cf991985a12659a0462691c542af6ba71648
Brian Hoppus <brian@hoppus.io>
2015-09-19 08:45:52 -0700

    Make Movie class explicitly inheritate from Object class

--- media.py
+++ media.py
@@ -1,6 +1,6 @@
 import webbrowser
 
-class Movie():
+class Movie(Object):
     """ This class provides a way to store movie related information"""
 
     def __init__(self, movie_title, movie_storyline, poster_image,
